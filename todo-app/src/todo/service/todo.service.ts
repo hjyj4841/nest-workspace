a389a797df10c50707b941d9c2b1ed0305fc8bf1
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-03 21:10:41 +0900

    nest js - prisma 활용(mysql 연동) crud

---
+++ todo-app/src/todo/service/todo.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { Todo } from '@prisma/client';
+
+@Injectable()
+export class TodoService {
+  constructor(private prismaService: PrismaService) {}
+
+  // 전체 조회
+  async fetchAllTodos(): Promise<Todo[]> {
+    return this.prismaService.todo.findMany();
+  }
+
+  // 단일 조회
+  async fetchTodoItem(id: number): Promise<Todo | null> {
+    return this.prismaService.todo.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+  }
+
+  // 삭제
+  async deleteTodoItem(id: number): Promise<Todo | null> {
+    return this.prismaService.todo.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+  }
+
+  // 수정
+  async updateTodoItem(
+    id: number,
+    title: string,
+    content: string | null,
+    is_done: boolean | null,
+  ): Promise<Todo | null> {
+    return this.prismaService.todo.update({
+      where: { id: Number(id) },
+      data: {
+        title: title,
+        content: content,
+        is_done: is_done,
+      },
+    });
+  }
+
+  // 추가가
+  async addTodoItem(data: Todo): Promise<Todo> {
+    return this.prismaService.todo.create({
+      data: data,
+    });
+  }
+}
